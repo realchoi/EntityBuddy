fac17c72c7fb1a274bb9130ceef7e98578a7f09f
Eric Choi <cai.desen@synyi.com>
2024-04-01 14:27:17 +0800

    测试数据库连接事件改为绑定 Command 的形式

--- DBuddy.AppUi/ViewModels/EntityGenerateViewModel.cs
+++ DBuddy.AppUi/ViewModels/EntityGenerateViewModel.cs
@@ -1,13 +1,19 @@
-﻿using AvaloniaEdit.Utils;
+﻿using System;
+using AvaloniaEdit.Utils;
 using DBuddy.Model.Enums;
 using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
 using DBuddy.Model;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using Npgsql;
 using SpringMountain.Infrastructure.Tools;
 
 namespace DBuddy.AppUi.ViewModels;
@@ -16,14 +22,18 @@ public class EntityGenerateViewModel : ViewModelBase
 {
     public EntityGenerateViewModel()
     {
-        DatabaseTypes.AddRange(EnumTool.GetAllMembers<DatabaseType>().Where(item => item.Value != 0)
-            .Select(item => new ComboBoxItemDto<int>((int)item.Value, item.Description)));
-        ProgrammingLanguages.AddRange(EnumTool.GetAllMembers<ProgrammingLanguage>().Where(item => item.Value != 0)
-            .Select(item => new ComboBoxItemDto<int>((int)item.Value, item.Description)));
+        InitComboBoxSource();
 
+        _selectedProgrammingLanguage = new ComboBoxItemDto<int>(-1, "请选择编程语言");
+        _selectedDatabaseType = new ComboBoxItemDto<int>(-1, "请选择数据库类型");
         _saveClassFilePath = string.Empty;
+
+        // 初始化事件命令
+        TestDbConnectCommand = ReactiveCommand.Create(TestDbConnect);
         SelectSaveClassFilePathCommand = ReactiveCommand.Create(SelectSaveClassFilePath);
     }
+
+    #region 下拉选项数据源
 
     /// <summary>
     /// 数据库类型下拉选项
@@ -34,6 +44,54 @@ public class EntityGenerateViewModel : ViewModelBase
     /// 编程语言下拉选项
     /// </summary>
     public ObservableCollection<ComboBoxItemDto<int>> ProgrammingLanguages { get; } = [];
+
+    #endregion
+
+    #region 双向绑定的属性
+
+    /// <summary>
+    /// 填写的数据库连接字符串
+    /// </summary>
+    private string _connectionString;
+
+    /// <summary>
+    /// 填写的数据库连接字符串
+    /// </summary>
+    public string ConnectionString
+    {
+        get => _connectionString;
+        set => this.RaiseAndSetIfChanged(ref _connectionString, value);
+    }
+
+    /// <summary>
+    /// 选择的编程语言
+    /// </summary>
+    private ComboBoxItemDto<int> _selectedProgrammingLanguage;
+
+    /// <summary>
+    /// 选择的编程语言
+    /// </summary>
+    public ComboBoxItemDto<int> SelectedProgrammingLanguage
+    {
+        get => _selectedProgrammingLanguage;
+        set => this.RaiseAndSetIfChanged(ref _selectedProgrammingLanguage, value);
+    }
+
+    /// <summary>
+    /// 选择的数据库类型
+    /// </summary>
+    private ComboBoxItemDto<int> _selectedDatabaseType;
+
+    /// <summary>
+    /// 选择的数据库类型
+    /// </summary>
+    public ComboBoxItemDto<int> SelectedDatabaseType
+    {
+        get => _selectedDatabaseType;
+        set => this.RaiseAndSetIfChanged(ref _selectedDatabaseType, value);
+    }
+
+    #endregion
 
     #region Class 文件保存路径
 
@@ -47,13 +105,59 @@ public class EntityGenerateViewModel : ViewModelBase
 
     #endregion
 
-
     #region Command
 
     public ReactiveCommand<Unit, Unit> SelectSaveClassFilePathCommand { get; }
+    public ReactiveCommand<Unit, Unit> TestDbConnectCommand { get; }
 
     #endregion
 
+
+    #region 私有方法
+
+    /// <summary>
+    /// 测试数据库连接
+    /// </summary>
+    private async void TestDbConnect()
+    {
+        if (ConnectionString.IsNullOrWhiteSpace())
+        {
+            var infoBox = MessageBoxManager.GetMessageBoxStandard("提示", "连接字符串不能为空！", ButtonEnum.Ok, Icon.Warning);
+            await infoBox.ShowAsync();
+            return;
+        }
+
+        var databaseTypeEnum = (DatabaseType)SelectedDatabaseType!.Value;
+        switch (databaseTypeEnum)
+        {
+            case DatabaseType.PostgreSql:
+                var errorMsg = await TryConnectPostgreSqlAsync(ConnectionString!);
+                if (!errorMsg.IsNullOrEmpty())
+                {
+                    var errorBox = MessageBoxManager.GetMessageBoxStandard("提示", $"连接失败！\r\n{errorMsg}",
+                        ButtonEnum.Ok, Icon.Error);
+                    await errorBox.ShowAsync();
+                }
+                else
+                {
+                    var successBox = MessageBoxManager.GetMessageBoxStandard("提示", "连接成功！\r\n现在可以生成实体了。",
+                        ButtonEnum.Ok, Icon.Success);
+                    await successBox.ShowAsync();
+                }
+
+                break;
+            case DatabaseType.Unknown:
+            case DatabaseType.Oracle:
+            case DatabaseType.MySql:
+            case DatabaseType.SqlServer:
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 选择 Class 文件保存路径
+    /// </summary>
     private async void SelectSaveClassFilePath()
     {
         // var dialog = new OpenFolderDialog
@@ -79,4 +183,57 @@ public class EntityGenerateViewModel : ViewModelBase
             SaveClassFilePath = result[0].Path.AbsolutePath;
         }
     }
+
+
+    /// <summary>
+    /// 初始化下拉选项数据源
+    /// </summary>
+    private void InitComboBoxSource()
+    {
+        var databaseTypes = EnumTool.GetAllMembers<DatabaseType>()
+            .Where(item => item.Value != 0)
+            .Select(item => new ComboBoxItemDto<int>((int)item.Value, item.Description));
+        DatabaseTypes.AddRange(databaseTypes);
+
+        var programmingLanguages = EnumTool.GetAllMembers<ProgrammingLanguage>()
+            .Where(item => item.Value != 0)
+            .Select(item => new ComboBoxItemDto<int>((int)item.Value, item.Description));
+        ProgrammingLanguages.AddRange(programmingLanguages);
+    }
+
+
+    /// <summary>
+    /// 尝试连接 PostgreSQL 数据库
+    /// </summary>
+    /// <param name="connectionString">PostgreSQL 连接字符串</param>
+    /// <returns>错误信息，为空则表示连接成功</returns>
+    private static async Task<string?> TryConnectPostgreSqlAsync(string connectionString)
+    {
+        NpgsqlConnection? conn = null;
+        try
+        {
+            if (connectionString.StartsWith("postgres://"))
+            {
+                connectionString = DbConnectionTool.GetPgSqlConnStr(connectionString);
+            }
+
+            await using (conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+            }
+
+            return null;
+        }
+        catch (Exception ex)
+        {
+            if (conn != null && conn.State != ConnectionState.Closed)
+            {
+                await conn.CloseAsync();
+            }
+
+            return ex.Message;
+        }
+    }
+
+    #endregion
 }
